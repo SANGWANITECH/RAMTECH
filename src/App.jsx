192710fb20558fd31a501953f4f7805c6604f938
Arthur Nkhoma <arthurnkhoma78@gmail.com>
2025-09-28 02:22:17 +0200

    commited

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { NavBar } from './NavBar'
+import { Hero } from './Hero'
+import Services from './Services'
+import Footer1 from './Footer1'
+
+function App() {
+  
+  return (
+    <>
+       <NavBar/>
+       <Hero/>
+       <Services/>
+       <Footer1/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Footer1.jsx
@@ -0,0 +1,43 @@
+
+const Footer1 = () => {
+    return(
+        <>
+            <div className="flex gap-40 justify-center mt-10">
+                <div className="w-100 flex flex-col gap-5">
+                    <h2 className="font-bold text-4xl">RAMTech</h2>
+                    <p>RAB Technology is a forward-thinking technology
+                    solutions company that provides reliable
+                    and innovative IT training services to businesses
+                    and individuals.
+                    </p>
+                    <div>
+                        <a href="#"><img src="#" alt="" /></a>
+                        <a href="#"><img src="#" alt="" /></a>
+                        <a href="#"><img src="#" alt="" /></a>
+                        <a href="#"><img src="#" alt="" /></a>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-5">
+                    <p><b>Legal issues</b></p>
+                    <p>Terms & Conditions</p>
+                    <p>Company License</p>
+                    <p>Data Protection & <br />Usage</p>
+                    <p>User Protection</p>
+                </div>
+                <div className="flex flex-col gap-5">
+                    <p><b>Company</b></p>
+                    <p>About us</p>
+                    <p>Portifolio</p>
+                    <p>Services</p>
+                    <p>Contact US</p>
+                </div>
+                <div className="flex flex-col gap-5">
+                    <p><b>Services</b></p>
+                    <a href=""></a>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer1;
